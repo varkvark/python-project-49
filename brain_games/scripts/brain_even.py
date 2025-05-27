96e1a4f62b5acfa7f776d63b7994d1792e28fb84
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-05-27 23:26:02 +0300

    brain_games/scripts/brain_even.py has been edited

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,39 +1,9 @@
-from brain_games.cli import welcome_user
-from random import randint
-
-
-def is_even(number):
-    return number % 2 == 0
+from brain_games.engine import launch
+from brain_games.games import brain_even
 
 
 def main():
-    user_name = welcome_user()
-
-
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-
-    score = 0
-    while score < 3:
-        random_int = randint(0, 100)
-        print('Question: ', random_int)
-
-
-        user_answer = input('Your answer: ').strip().lower()
-        correct_answer = 'yes' if is_even(random_int) else 'no'
-
-
-        if user_answer == correct_answer:
-            print('Correct!')
-            score += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(.", end=' ')
-            print(f"Correct answer was '{correct_answer}'")
-            break
-
-
-    if score == 3:
-        print('Congratulations, ', user_name)
+    launch(brain_even)
 
 
 if __name__ == "__main__":
