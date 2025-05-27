61c01f90f6734da800b1474604ce48ca263fd7c6
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-05-27 23:32:23 +0300

    add brain_games/games package

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,23 @@
+from random import randint
+from random import choice
+
+
+TERMS = 'What is the result of the expression?'
+
+
+def questions_and_answers():
+    random_int1 = randint(0, 100)
+    random_int2 = randint(0, 100)
+    operation = choice(['+', '-', '*'])
+    question = f'{random_int1} {operation} {random_int2}'
+
+
+    if operation == '+':
+        correct_answer = str(random_int1 + random_int2)
+    elif operation == '-':
+        correct_answer = str(random_int1 - random_int2)
+    elif operation == '*':
+        correct_answer = str(random_int1 * random_int2)
+
+
+    return question, correct_answer
---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+TERMS = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def questions_and_answers():
+    question = randint(0, 100)
+    correct_answer = 'yes' if is_even(question) else 'no'
+    return question, correct_answer
