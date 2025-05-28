e94f3e1cfddc7530e11b2cff55c05d0d389b0e2e
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-05-29 00:17:17 +0300

    add  brain_games/games/brain_gcd.py

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+TERMS = 'Find the greatest common divisor of given numbers.'
+
+
+def gcd(a, b):
+    if a ==0 or b == 0:
+        result = abs(a - b)
+    while b != 0:
+        remainder = a % b
+        a = b
+        b = remainder
+    result = a
+    return result
+
+
+def questions_and_answers():
+    random_int1 = randint(0, 20)
+    random_int2 = randint(0, 20)
+    question = f'{random_int1} {random_int2}'
+    correct_answer = str(gcd(random_int1, random_int2))
+    return question, correct_answer
