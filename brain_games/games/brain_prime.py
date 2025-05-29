0cbda203833a9bbac5d1cdc55c3bf02078b32571
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-05-29 22:20:00 +0300

    Add brain_games/games/brain_prime.py

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,19 @@
+from random import randint
+
+
+TERMS = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    prime_list = []
+    for index in range(1, number + 1):
+        if number % index == 0:
+            prime_list.append(index)
+    return(prime_list)
+
+
+def questions_and_answers():
+    question = randint(0, 100)
+    correct_answer = 'yes' if len(is_prime(question)) == 2 else 'no'
+
+    return question, correct_answer
